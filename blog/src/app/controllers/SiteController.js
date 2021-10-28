80be381908eb46bc34bcbf3c48ea0f43b2d5d08f
raito <hatran12387@gmail.com>
2021-10-28 15:08:46 +0700

    create routes, controllers folder

---
+++ blog/src/app/controllers/SiteController.js
@@ -0,0 +1,14 @@
+class Sitecontroller {
+
+    // [GET]/
+    index(req, res) {
+        res.render('home');
+    }
+
+    // [GET] /search
+    search(req, res) {
+        res.render('search')
+    }
+}
+
+module.exports = new Sitecontroller;
